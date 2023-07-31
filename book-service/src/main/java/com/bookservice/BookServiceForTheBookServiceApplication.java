039d1c0339382345884c0432dc6fbdf13b43822d
Sabir Mammadov <sabir.mammadov@icloud.com>
2023-07-31 22:42:37 +0200

    initializing Spring Boot project for Book and User service

---
+++ book-service/src/main/java/com/bookservice/BookServiceForTheBookServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bookservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookServiceForTheBookServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookServiceForTheBookServiceApplication.class, args);
+	}
+
+}
