662e21a86e12b06e3899b8adfc16f06c3c8bf149
Sabir Mammadov <sabir.mammadov@icloud.com>
2023-08-01 19:56:38 +0200

    Created routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { BookFormComponent } from './book-form/book-form.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'books', component: BookListComponent },
+  { path: 'books/:id', component: BookDetailComponent },
+  { path: 'books/:id/edit', component: BookFormComponent },
+  { path: 'books/new', component: BookFormComponent },
+  { path: 'register', component: UserRegistrationComponent },
+  { path: 'login', component: UserLoginComponent },
+  { path: '', redirectTo: '/books', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
